c14e636983c05fe502964e28caeb04867e461ba2
Glauter Santos <glauterlima@gmail.com>
2018-06-29 20:21:59 -0300

    3.10 concluído - Validando valores inválidos com Bean Validation

--- aletheia/src/main/java/br/jus/trf1/aletheia/model/Sistema.java
+++ aletheia/src/main/java/br/jus/trf1/aletheia/model/Sistema.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 @Table(name = "sistema")
@@ -14,6 +16,8 @@ public class Sistema {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long codigo;
 	
+	@NotNull
+	@Size(min = 3, max = 20)
 	private String nome;
 
 	
--- aletheia/src/main/java/br/jus/trf1/aletheia/resource/SistemaResource.java
+++ aletheia/src/main/java/br/jus/trf1/aletheia/resource/SistemaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -31,7 +32,7 @@ public class SistemaResource {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Sistema> criar(@RequestBody Sistema sistema, HttpServletResponse response){
+	public ResponseEntity<Sistema> criar(@Valid @RequestBody Sistema sistema, HttpServletResponse response){
 		Sistema sistemaSalvo = sistemaRepository.save(sistema);	
 		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
