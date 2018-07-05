a0650b16fdc52f3586a613b77964569f3380fd1f
Glauter Santos <glauterlima@gmail.com>
2018-07-05 19:10:09 -0300

    4.1 Removendo sistema com DELETE

--- aletheia/src/main/java/br/jus/trf1/aletheia/resource/SistemaResource.java
+++ aletheia/src/main/java/br/jus/trf1/aletheia/resource/SistemaResource.java
@@ -1,21 +1,24 @@
 package br.jus.trf1.aletheia.resource;
 
-import java.net.URI;
 import java.util.List;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import br.jus.trf1.aletheia.event.RecursoCriadoEvent;
 import br.jus.trf1.aletheia.model.Sistema;
 import br.jus.trf1.aletheia.repository.SistemaRepository;
 
@@ -26,6 +29,9 @@ public class SistemaResource {
 	@Autowired
 	private SistemaRepository sistemaRepository;
 	
+	@Autowired
+	private ApplicationEventPublisher publisher;
+	
 	@GetMapping
 	public List<Sistema> listar() {
 		return sistemaRepository.findAll();
@@ -35,17 +41,21 @@ public class SistemaResource {
 	public ResponseEntity<Sistema> criar(@Valid @RequestBody Sistema sistema, HttpServletResponse response){
 		Sistema sistemaSalvo = sistemaRepository.save(sistema);	
 		
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{codigo}")
-			.buildAndExpand(sistemaSalvo.getCodigo()).toUri();
-		response.setHeader("Location", uri.toASCIIString());
+		publisher.publishEvent(new RecursoCriadoEvent(this, response, sistemaSalvo.getCodigo()));
 		
-		return ResponseEntity.created(uri).body(sistemaSalvo); 
+		return ResponseEntity.status(HttpStatus.CREATED).body(sistemaSalvo); 
 	}
 	
 	@GetMapping("/{codigo}")
 	public ResponseEntity<Sistema> buscarPeloCodigo(@PathVariable Long codigo){
 		Sistema sistema = sistemaRepository.findOne(codigo);
 		return sistema != null ? ResponseEntity.ok(sistema) : ResponseEntity.notFound().build();
+	}
+	
+	@DeleteMapping("/{codigo}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void remover(@PathVariable Long codigo){
+		sistemaRepository.delete(codigo);
 	}
 
 }
