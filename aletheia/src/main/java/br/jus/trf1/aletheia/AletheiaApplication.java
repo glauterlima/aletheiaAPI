cd746dd7ad879f0a6821cbde45ff704f5016a5a6
Glauter Santos <glauterlima@gmail.com>
2018-08-16 21:27:20 -0300

    7.2 - concluido - Profiles do Spring

--- aletheia/src/main/java/br/jus/trf1/aletheia/AletheiaApplication.java
+++ aletheia/src/main/java/br/jus/trf1/aletheia/AletheiaApplication.java
@@ -2,8 +2,12 @@ package br.jus.trf1.aletheia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import br.jus.trf1.aletheia.config.property.AletheiaProperty;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AletheiaProperty.class)
 public class AletheiaApplication {
 
 	public static void main(String[] args) {
