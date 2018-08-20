d2d97b12025c145b0025ad56f2cdfcf0cab4333c
Glauter Santos <glauterlima@gmail.com>
2018-08-20 18:34:30 -0300

    Fazendo deploy no Heroku

--- aletheia/src/main/java/br/jus/trf1/aletheia/cors/CorsFilter.java
+++ aletheia/src/main/java/br/jus/trf1/aletheia/cors/CorsFilter.java
@@ -39,7 +39,7 @@ public class CorsFilter implements Filter {
 		if ("OPTIONS".equals(request.getMethod()) && aletheiaProperty.getOriginPermitida().equals(request.getHeader("Origin"))) {
 			response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
         	response.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
-        	response.setHeader("Access-Control-Max-Age", "3600");
+        	response.setHeader("Access-Control-Max-Age", "3601");
 			
 			response.setStatus(HttpServletResponse.SC_OK);
 		} else {
